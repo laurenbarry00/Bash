6058db3ec24d64945afed7395a7ab0135b77bdd1
ELIZABETH MIN <elmm682@gmail.com>
2018-09-14 19:11:40 -0700

    testing gradle patch

--- src/main/java/BotRunner.java
+++ src/main/java/BotRunner.java
@@ -1,20 +1,12 @@
-import net.dv8tion.jda.core.JDA;
-import net.dv8tion.jda.core.JDABuilder;
-import org.slf4j.Logger;
-
-import java.io.BufferedReader;
-import java.io.File;
-import java.io.FileReader;
-import java.io.IOException;
-
 public class Main {
     public static void main(String[] args) throws Exception {
         String token = "-1";
         try {
-            final File tokenFile = new File("token.txt");
-            BufferedReader br = new BufferedReader(new FileReader(tokenFile));
-            token = br.readLine();
-            br.close();
+            // final File tokenFile = new File("token.txt");
+            // BufferedReader br = new BufferedReader(new FileReader(tokenFile));
+            // token = br.readLine();
+            // br.close();
+            System.out.println("Success"); 
         } catch (IOException e) {
            System.out.println("Error loading token from file:");
            e.printStackTrace();
